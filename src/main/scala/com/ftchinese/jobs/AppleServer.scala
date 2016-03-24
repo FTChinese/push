fe2a560fdf69a0bb17608c02549ffadab6f94f8a
wanbo <gewanbo@gmail.com>
2016-03-24 14:26:32 +0800

    Apple server.

---
+++ src/main/scala/com/ftchinese/jobs/AppleServer.scala
@@ -0,0 +1,13 @@
+package com.ftchinese.jobs
+
+/**
+ * Apple server.
+ * Created by wanbo on 16/3/21.
+ */
+object AppleServer {
+    val pro_host = "gateway.push.apple.com"
+    val pro_port = 2195
+
+    val dev_host = "gateway.sandbox.push.apple.com"
+    val dev_port = 2195
+}
