a34061cd5fc78eba0103a7003148d74bba02db83
wanbo <gewanbo@gmail.com>
2016-03-25 14:24:36 +0800

    Task message.

--- src/main/scala/com/ftchinese/jobs/common/TaskMessage.scala
+++ src/main/scala/com/ftchinese/jobs/common/TaskMessage.scala
@@ -3,5 +3,8 @@ package com.ftchinese.jobs.common
 /**
  * Task case class
  * Created by wanbo on 16/3/23.
+ *
+ * @param production: Default is true, if false the task just for testing.
+ * @param createTime: The time millis when task was created.
  */
-case class TaskMessage(message: String = "", action: String = "", id: String = "")
+case class TaskMessage(message: String = "", action: String = "", id: String = "", production: Boolean = true, createTime: Int = System.currentTimeMillis())
