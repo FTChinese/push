19e1e764b1ce778454b3be88457b690503e2589b
wanbo <gewanbo@gmail.com>
2016-03-25 15:06:08 +0800

    Complete notification message construct.

--- src/main/scala/com/ftchinese/jobs/common/NotificationMessage.scala
+++ src/main/scala/com/ftchinese/jobs/common/NotificationMessage.scala
@@ -3,11 +3,14 @@ package com.ftchinese.jobs.common
 /**
  * The message of Notification.
  * Created by wanbo on 3/22/16.
+ *
+ * @param   id: The notification unique id, at least it is unique in the notifications created current date.
  */
 case class NotificationMessage( deviceToken: String = "", deviceType: String = "", appNumber: String = "", timeZone: String = "",
-      text: String = "", action: String = "", id: String = "", lead: String = "", badge: String = "1", sound: String = "default" ) extends Message{
+      text: String = "", action: String = "", label: String = "", lead: String = "", badge: String = "1", sound: String = "default",
+                                 id: Int, production: Boolean, createTime: Long) extends Message{
 
       override def toJson: String ={
-            """{"deviceToken": "%s", "deviceType": "%s", "appNumber": "%s", "timeZone": "%s", "text": "%s", "action": "%s", "id": "%s", "lead": "%s", "badge": "%s", "sound": "%s"}""".format(deviceToken, deviceType, appNumber, timeZone, text, action, id, lead, badge, sound)
+            """{"deviceToken": "%s", "deviceType": "%s", "appNumber": "%s", "timeZone": "%s", "text": "%s", "action": "%s", "label": "%s", "lead": "%s", "badge": "%s", "sound": "%s", "id": %d, "production": %s, "createTime": %s}""".format(deviceToken, deviceType, appNumber, timeZone, text, action, label, lead, badge, sound, id, production, createTime)
       }
 }
