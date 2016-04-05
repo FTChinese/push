ed7cdaba00c8c4ad5c1aff7cd305d2dc85683f9b
wanbo <gewanbo@gmail.com>
2016-04-05 14:18:26 +0800

    The trait of Apple servers.

---
+++ src/main/scala/com/ftchinese/jobs/common/AppleServer.scala
@@ -0,0 +1,13 @@
+package com.ftchinese.jobs.common
+
+/**
+  * Apple server
+  * Created by wanbo on 16/4/5.
+  */
+trait AppleServer {
+    val pro_host: String = ""
+    val pro_port: Int = _
+
+    val dev_host: String = ""
+    val dev_port: Int = _
+}
